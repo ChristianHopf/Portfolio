cc918bd07f090591aced89e34f0fd6467abd00fe
ChristianHopf <cehopf@gmail.com>
2024-09-12 17:54:03 -0400

    Centered About text

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="flex flex-col items-center md:items-start md:flex-row py-2 gap-8">
+    <div className="flex justify-center md:items-start md:flex-row py-2 gap-8">
       {/* <Image
         src="/static/images/googlepfp.jpg"
         alt="me"
@@ -10,14 +10,12 @@ export default function About() {
         height={300}
         className="rounded-full items-center shadow-2xl"
       /> */}
-      <div className="flex flex-col">
-        {/* <h1 className="text-text-primary-light text-4xl font-semibold md:text-7xl  mt-8">
+      {/* <h1 className="text-text-primary-light text-4xl font-semibold md:text-7xl  mt-8">
           Christian Hopf
         </h1> */}
-        <p className="text-stone-700 text-2xl md:text-4xl mt-2">
-          Full stack developer using modern JavaScript + TypeScript frameworks.
-        </p>
-      </div>
+      <p className="text-stone-700 text-2xl md:text-4xl mt-2">
+        Full stack developer using modern JavaScript + TypeScript frameworks.
+      </p>
     </div>
   );
 }
