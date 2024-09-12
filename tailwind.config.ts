10c6b9706f140b885ce1f1258fb0afb2b368f3e2
ChristianHopf <cehopf@gmail.com>
2024-09-11 23:17:58 -0400

    Following next/font with tailwind doc

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["var(--font-hack)"],
       },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
     // colors: {
     //   // "header-dark": "#00132D",
