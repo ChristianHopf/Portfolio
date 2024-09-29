6cde5f023c1d22171ee2066bea3fb1b0067a0459
ChristianHopf <cehopf@gmail.com>
2024-09-28 21:26:48 -0400

    Cards are columns, rows on xl screens

--- app/components/QuizzingtonCard.tsx
+++ app/components/QuizzingtonCard.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Image from "next/image";
 import Link from "next/link";
-import Carousel from "./ui/Carousel";
 
 type Props = {};
 
@@ -14,25 +12,27 @@ function QuizzingtonCard({}: Props) {
   ];
 
   return (
-    <div className="flex flex-col lg:flex-row w-full mx-auto justify-center py-16 mb-8 items-center rounded-md gap-4">
-      <div className="flex items-center">
+    <div className="flex flex-col xl:flex-row w-full mx-auto justify-center mb-2 lg:mb-8 items-center rounded-md gap-4">
+      <div className="flex items-center mx-6 md:mx-0">
         <img key={0} src={images[0]} alt={`Image ${0}`} />
       </div>
-      <div className="border-2 border-[#0A84FF] self-stretch"></div>
-      <div className="flex flex-col justify-between w-full px-2 self-stretch">
+      <div className="border-2 border-[#0A84FF] self-stretch mx-6 md:mx-0"></div>
+      <div className="flex flex-col justify-between xl:w-full mx-6 md:mx-0">
         <div className="flex flex-col">
-          <h1 className="text-6xl font-semibold mb-2">Quizzington</h1>
-          <p className="text-stone-700 text-2xl">
+          <h1 className="text-4xl md:text-5xl font-semibold mb-2">
+            Quizzington
+          </h1>
+          <p className="text-stone-700 text-xl md:text-2xl">
             Quizzington is a NextJS app for creating custom quizzes. Data is
             stored and fetched with Vercel Postgres.
           </p>
         </div>
-        <div className="flex flex-col gap-4 items-start pb-2">
+        <div className="flex flex-col items-start gap-4 mt-4 pb-2">
           <Link
             href="https://quizzington.vercel.app/"
             rel="noopener noreferrer"
             target="_blank"
-            className="text-[#0A84FF] hover:text-bg-light text-xl link-underline"
+            className="text-[#0A84FF] hover:text-bg-light text-lg xl:text-xl link-underline"
           >
             https://quizzington.vercel.app/
           </Link>
@@ -40,7 +40,7 @@ function QuizzingtonCard({}: Props) {
             href={"https://github.com/ChristianHopf/Quizzington"}
             rel="noopener noreferrer"
             target="_blank"
-            className="text-[#0A84FF] hover:text-bg-light text-xl link-underline"
+            className="text-[#0A84FF] hover:text-bg-light text-lg xl:text-xl link-underline"
           >
             View GitHub Repo
           </Link>
