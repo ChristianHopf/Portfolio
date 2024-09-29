40826db873e478f0c27d7c76ada0d1181d083cbd
ChristianHopf <cehopf@gmail.com>
2024-09-28 21:37:49 -0400

    Responsive bottom margin on about section

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col ">
       <div className="container py-8 mx-auto">
-        <section className="mb-24">
+        <section className="mb-6 md:mb-24">
           <About />
         </section>
         <section id="works" className="relative flex flex-col w-full mb-4">
